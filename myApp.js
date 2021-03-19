f3285f34cf855cf5f1b03b91ecbd08ef46c3c9f5
Life-orb <75528577+Life-orb@users.noreply.github.com>
2021-03-19 00:16:06 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) =>
   app.get("/json", function(req, res) {
 
 
-  if (process.env.MESSAGE_STYLE === UPPERCASE)
+  if (process.env.MESSAGE_STYLE === uppercase)
   {
     res.json({"message": "HELLO JSON"})
 
