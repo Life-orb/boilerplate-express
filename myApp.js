3b3e0825ac0f9440451a3c9411e5fac3cbde0813
Life-orb <75528577+Life-orb@users.noreply.github.com>
2021-03-18 23:29:25 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + "/public/style.css")) ;
+app.use(express.static(__dirname + "/public")) ;
+app.use("/public")
 app.get("/", function(req, res) {
     res.sendFile( __dirname + '/views/index.html');
   });
