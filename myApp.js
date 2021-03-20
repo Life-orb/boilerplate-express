3f93067e3ff6c9cb8d56cbedfbb7dffbed50581e
Life-orb <75528577+Life-orb@users.noreply.github.com>
2021-03-20 14:28:07 -0300

    bodyparser

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 const { json } = require('body-parser');
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(__dirname + "/public")) ;
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) =>
